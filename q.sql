df5d919ff62dbe65f68d7ea903d7eaa46e1f6ada
coderperson-tech <rich.sam1209@gmail.com>
2025-10-01 18:06:39 +0400

    commit1

---
+++ q.sql
@@ -0,0 +1,18 @@
+SELECT SUM(salary) AS total_salary
+FROM Employees;
+
+
+SELECT AVG(salary) AS average_salary
+FROM Employees;
+
+
+SELECT COUNT(DISTINCT department) AS total_departments
+FROM Employees;
+
+
+SELECT MIN(salary) AS minimum_salary
+FROM Employees;
+
+
+SELECT MAX(salary) AS maximum_salary
+FROM Employees;
